d501ce04fdd46e479dba0fefe78be2e2347a4fcb
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-05-17 21:41:23 +0200

    Refactor: Aligned the code to the 'book-preview.js' in order to let the web component fire the preview when pressed

--- scripts.js
+++ scripts.js
@@ -55,8 +55,15 @@ const applyTheme = (theme) => {
 // Function to update "Show more" button text and state
 const updateShowMoreButton = () => {
   const remainingBooks = matches.length - page * BOOKS_PER_PAGE;
-  const button = document.querySelector('show-more-button');
-  button.update(remainingBooks);
+  const button = getElement('[data-list-button]');
+  button.innerText = `Show more (${remainingBooks})`;
+  button.disabled = remainingBooks <= 0;
+  button.innerHTML = `
+    <span>Show more</span>
+    <span class="list__remaining">(${
+      remainingBooks > 0 ? remainingBooks : 0
+    })</span>
+  `;
 };
 
 // Event listener functions
@@ -72,89 +79,93 @@ const openOverlay = (selector, focusSelector = null) => {
 const applySearchFilters = (filters) => {
   return books.filter((book) => {
     const titleMatch =
-      filters.title.trim() === "" ||
+      filters.title.trim() === '' ||
       book.title.toLowerCase().includes(filters.title.toLowerCase());
     const authorMatch =
-      filters.author === "any" || book.author === filters.author;
+      filters.author === 'any' || book.author === filters.author;
     const genreMatch =
-      filters.genre === "any" || book.genres.includes(filters.genre);
+      filters.genre === 'any' || book.genres.includes(filters.genre);
     return titleMatch && authorMatch && genreMatch;
   });
 };
 
 // Event listeners
-getElement("[data-search-cancel]").addEventListener("click", () =>
-  closeOverlay("[data-search-overlay]")
+getElement('[data-search-cancel]').addEventListener('click', () =>
+  closeOverlay('[data-search-overlay]')
 );
-getElement("[data-settings-cancel]").addEventListener("click", () =>
-  closeOverlay("[data-settings-overlay]")
+getElement('[data-settings-cancel]').addEventListener('click', () =>
+  closeOverlay('[data-settings-overlay]')
 );
-getElement("[data-header-search]").addEventListener("click", () =>
-  openOverlay("[data-search-overlay]", "[data-search-title]")
+getElement('[data-header-search]').addEventListener('click', () =>
+  openOverlay('[data-search-overlay]', '[data-search-title]')
 );
-getElement("[data-header-settings]").addEventListener("click", () =>
-  openOverlay("[data-settings-overlay]")
+getElement('[data-header-settings]').addEventListener('click', () =>
+  openOverlay('[data-settings-overlay]')
 );
-getElement("[data-list-close]").addEventListener("click", () =>
-  closeOverlay("[data-list-active]")
+getElement('[data-list-close]').addEventListener('click', () =>
+  closeOverlay('[data-list-active]')
 );
 
-getElement("[data-settings-form]").addEventListener("submit", (event) => {
+getElement('[data-settings-form]').addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const { theme } = Object.fromEntries(formData);
   applyTheme(theme);
-  closeOverlay("[data-settings-overlay]");
+  closeOverlay('[data-settings-overlay]');
 });
 
-getElement("[data-search-form]").addEventListener("submit", (event) => {
+getElement('[data-search-form]').addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const filters = Object.fromEntries(formData);
   matches = applySearchFilters(filters);
   page = 1;
-  getElement("[data-list-message]").classList.toggle(
-    "list__message_show",
+  getElement('[data-list-message]').classList.toggle(
+    'list__message_show',
     matches.length < 1
   );
-  getElement("[data-list-items]").innerHTML = "";
+  getElement('[data-list-items]').innerHTML = '';
   createBookPreviews(
     matches.slice(0, BOOKS_PER_PAGE),
-    getElement("[data-list-items]")
+    getElement('[data-list-items]')
   );
   updateShowMoreButton();
-  window.scrollTo({ top: 0, behavior: "smooth" });
-  closeOverlay("[data-search-overlay]");
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  closeOverlay('[data-search-overlay]');
 });
 
-getElement("[data-list-button]").addEventListener("click", () => {
+getElement('[data-list-button]').addEventListener('click', () => {
   createBookPreviews(
     matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE),
-    getElement("[data-list-items]")
+    getElement('[data-list-items]')
   );
   page += 1;
   updateShowMoreButton();
 });
 
-getElement("[data-list-items]").addEventListener("click", (event) => {
-  const pathArray = Array.from(event.composedPath());
-  const active = pathArray.find((node) => node?.dataset?.preview);
-  if (active) {
-    const book = books.find((book) => book.id === active.dataset.preview);
-    if (book) {
-      document.querySelector('book-detail').open(book);
-    }
+getElement('[data-list-items]').addEventListener('book-preview-click', (event) => {
+  const bookId = event.detail.id;
+  const book = books.find((book) => book.id === bookId);
+  if (book) {
+    getElement('[data-list-active]').open = true;
+    getElement('[data-list-blur]').src = book.image;
+    getElement('[data-list-image]').src = book.image;
+    getElement('[data-list-title]').innerText = book.title;
+    getElement('[data-list-subtitle]').innerText = `${
+      authors[book.author]
+    } (${new Date(book.published).getFullYear()})`;
+    getElement('[data-list-description]').innerText = book.description;
   }
 });
 
 // Initial setup
-createOptions(genres, "All Genres", getElement("[data-search-genres]"));
-createOptions(authors, "All Authors", getElement("[data-search-authors]"));
+createOptions(genres, 'All Genres', getElement('[data-search-genres]'));
+createOptions(authors, 'All Authors', getElement('[data-search-authors]'));
 applyTheme(
-  window.matchMedia("(prefers-color-scheme: dark)").matches ? "night" : "day"
+  window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
 );
 createBookPreviews(
   matches.slice(0, BOOKS_PER_PAGE),
-  getElement("[data-list-items]")
+  getElement('[data-list-items]')
 );
 updateShowMoreButton();
